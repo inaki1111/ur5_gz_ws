d86969666850c7386b75fadfacbbd801dc4770fd
Iñaki Roman <inakirm111@gmail.com>
2025-04-25 10:40:18 +0200

    Delete build directory

--- build/ur_moveit_config/colcon_command_prefix_build.sh
+++
@@ -1,2 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/cromanma/ur5_gz_ws/install/ur_description/share/ur_description/package.sh"
--- build/ur_simulation_gz/colcon_command_prefix_build.sh
+++
@@ -1,3 +0,0 @@
-# generated from colcon_core/shell/template/command_prefix.sh.em
-. "/home/cromanma/ur5_gz_ws/install/ur_description/share/ur_description/package.sh"
-. "/home/cromanma/ur5_gz_ws/install/ur_moveit_config/share/ur_moveit_config/package.sh"
